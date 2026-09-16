63755f5efd457cab3c26f9a0756f41c9b498bbec
civarc <civarcops@gmail.com>
2026-09-16 10:13:04 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const menuButton = document.getElementById('menuButton');
+const mainNav = document.getElementById('mainNav');
+
+if (menuButton && mainNav) {
+    menuButton.addEventListener('click', () => {
+        mainNav.classList.toggle('open');
+    });
+
+    mainNav.querySelectorAll('a').forEach(link => {
+        link.addEventListener('click', () => {
+            mainNav.classList.remove('open');
+        });
+    });
+}
